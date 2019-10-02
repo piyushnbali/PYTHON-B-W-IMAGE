b4046d659e5109fa5957eb0f753bda0ef8bf73ca
Aditya <adityab7@protonmail.com>
2019-10-03 01:22:58 +0530

    Add support for choosing a file at runtime
    Signed-off-by: Aditya <adityab7@protonmail.com>

--- black to white.py
+++ black to white.py
@@ -5,10 +5,19 @@ Created on Sun Jul 28 12:08:24 2019
 @author: pnbal
 """
 
-import cv2 
+import cv2, os
 
-img=cv2.imread('IMG_20190709_194910.jpg')
+print("Listing all images in the current directory.")
+ext = [".jpg",".jpeg"]
+for file in os.listdir('./'):
+    if file.endswith(tuple(ext)):
+        print(file)
+imgname = input("Enter the name of the image: ")
+img=cv2.imread(imgname)
 clahe=cv2.createCLAHE()
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 enh_img=clahe.apply(gray_img)
-cv2.imwrite('ech.png',enh_img)
+print("Saving the B/W image with name enh_%s in the current directory"%(imgname))
+if not cv2.imwrite("enh_%s"%(imgname), enh_img):
+    raise Exception("Failed")
+print("Done!")
